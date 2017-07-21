516af860dab5dee7c93b39092466f2318ed9147e
Mariusz Zaborski <oshogbo@vexillium.org>
2017-07-21 14:07:36 +0200

    Fix building NODEBUG version.

--- drivers/gpu/drm/i915/i915_guc_submission.c
+++ drivers/gpu/drm/i915/i915_guc_submission.c
@@ -1242,10 +1242,11 @@ static void guc_log_cleanup(struct intel_guc *guc)
 
 static int guc_log_create_extras(struct intel_guc *guc)
 {
-	struct drm_i915_private *dev_priv = guc_to_i915(guc);
+	struct drm_i915_private *dev_priv __unused;
 	void *vaddr;
 	int ret;
 
+	dev_priv = guc_to_i915(guc);
 	lockdep_assert_held(&dev_priv->drm.struct_mutex);
 
 	/* Nothing to do */
@@ -1358,9 +1359,10 @@ static void guc_log_create(struct intel_guc *guc)
 
 static int guc_log_late_setup(struct intel_guc *guc)
 {
-	struct drm_i915_private *dev_priv = guc_to_i915(guc);
+	struct drm_i915_private *dev_priv __unused;
 	int ret;
 
+	dev_priv = guc_to_i915(guc);
 	lockdep_assert_held(&dev_priv->drm.struct_mutex);
 
 	if (i915.guc_log_level < 0)
