c211b91567c369ef9e6e9ddff70bd4f66a32cf73
Johannes Lundberg <johalun0@gmail.com>
2018-01-26 11:35:32 +0000

    [lkpi] change copyright owner for new files

--- sys/compat/linuxkpi/common/include/linux/irqreturn.h
+++ sys/compat/linuxkpi/common/include/linux/irqreturn.h
@@ -1,5 +1,5 @@
 /*-
- * Copyright (c) 2017 Johannes Lundberg
+ * Copyright (c) 2017 Limelight Networks, Inc.
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
